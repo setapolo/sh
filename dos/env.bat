0255c7de6cf7252c4dcd08febbf26df694fd5832
Temp <Temp>
2016-05-30 01:05:06 +0900

    test

---
+++ dos/env.bat
@@ -0,0 +1,37 @@
+echo off
+echo	%%COMPUTERNAME%%	%COMPUTERNAME%
+echo	%%ComSpec%%	%ComSpec%
+echo	%%HOMEDRIVE%%	%HOMEDRIVE%
+echo	%%HOMEPATH%%	%HOMEPATH%
+echo	%%USERPROFILE%%	%USERPROFILE%
+echo		
+echo	%%OS%%	%OS%
+echo	%%PATH%%	%PATH%
+echo	%%PATHEXT%%	%PATHEXT%
+echo	%%RANDOM%%	%RANDOM%
+echo	%%SystemDrive%%	%SystemDrive%
+echo	%%SystemRoot%%	%SystemRoot%
+echo	%%TMP%%	%TMP%
+echo	%%TEMP%%	%TEMP%
+echo	%%windir%%	%windir%
+echo	%%ALLUSERSPROFILE%%	%ALLUSERSPROFILE%
+echo	%%APPDATA%%	%APPDATA%
+echo		
+echo	%%LOGONSERVER%%	%LOGONSERVER%
+echo	%%NUMBER_OF_PROCESSORS%%	%NUMBER_OF_PROCESSORS%
+echo	%%PROCESSOR_ARCHITECTURE%%	%PROCESSOR_ARCHITECTURE%
+echo	%%PROCESSOR_IDENTIFIER%%	%PROCESSOR_IDENTIFIER%
+echo	%%PROCESSOR_LEVEL%%	%PROCESSOR_LEVEL%
+echo	%%PROCESSOR_REVISION%%	%PROCESSOR_REVISION%
+echo	%%PROMPT%%	%PROMPT%
+echo	%%PSModulePath	%PSModulePath
+echo	%%PUBLIC%%	%PUBLIC%
+echo	%%SESSIONNAME%%	%SESSIONNAME%
+echo	%%USERDOMAIN%%	%USERDOMAIN%
+echo	%%username%%	%username%
+echo	%%USERPROFILE%%	%USERPROFILE%
+echo		
+echo	%%CommonProgramFiles%%	%CommonProgramFiles%
+echo	%%CommonProgramFiles(x86)%%	%CommonProgramFiles(x86)%
+echo	%%ProgramFiles%%	%ProgramFiles%
+echo	%%ProgramFiles(x86)%%	%ProgramFiles(x86)%
